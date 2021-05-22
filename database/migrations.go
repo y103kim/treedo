a6a3f506c9a68d589593dd607dcf23182ba36183
Youngwan Kim <y103.kim@gmail.com>
2021-05-23 02:22:56 +0900

    migrations: edge table

--- database/migrations.go
+++ database/migrations.go
@@ -21,7 +21,7 @@ func migrationV1(db *sql.DB) error {
 			updated_at  INTEGER                           NOT NULL DEFAULT (strftime('%s', 'now'))
 		)`
 	if _, err := db.Exec(createTodoTable); err != nil {
-		return errors.Wrap(err, "Cannot create table")
+		return errors.Wrap(err, "Cannot create todo table")
 	}
 
 	createUpdateTrigger := `
@@ -31,6 +31,25 @@ func migrationV1(db *sql.DB) error {
 			END`
 	if _, err := db.Exec(createUpdateTrigger); err != nil {
 		return errors.Wrap(err, "Cannot create update trigger")
+	}
+
+	createEdgeTable := `
+		CREATE TABLE edge (
+		  id    INTEGER PRIMEARY KEY,
+		  head  INTEGER NOT NULL,
+		  tail  INTEGER NOT NULL,
+		  root  INTEGER NOT NULL,
+
+		  FOREIGN KEY(head) REFERENCES todo (id) ON DELETE CASCADE
+		  FOREIGN KEY(tail) REFERENCES todo (id) ON DELETE CASCADE
+		  FOREIGN KEY(root) REFERENCES todo (id) ON DELETE CASCADE
+
+		  CHECK(head != tail)
+		  UNIQUE(head, tail)
+		  UNIQUE(root, tail)
+		)`
+	if _, err := db.Exec(createEdgeTable); err != nil {
+		return errors.Wrap(err, "Cannot create edge table")
 	}
 
 	return nil
