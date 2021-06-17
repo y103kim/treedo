e0f394f378a6ff9c79dcac60e586b9cd094a6488
Youngwan Kim <y103.kim@gmail.com>
2021-06-17 23:55:01 +0900

    tree/node: functions to implement BaseModel

--- database/base_model.go
+++ database/base_model.go
@@ -15,17 +15,17 @@ type BaseModel interface {
 	Deserialize(db_output string) error
 }
 
-func (db *Database) Insert(obj *BaseModel) error {
-	table := (*obj).GetTableName()
-	fields := (*obj).GetFieldNames()
-	values := (*obj).GetValueList()
+func (db *Database) Insert(obj BaseModel) error {
+	table := obj.GetTableName()
+	fields := obj.GetFieldNames()
+	values := obj.GetValueList()
 	return db.Tx(func(tx *sql.Tx) error {
 		r, err := tx.Exec("INSERT INTO ? (?) VALUES (?)", table, fields, values)
 		if err != nil {
 			return errors.Wrapf(err, "Cannot insert object %v", obj)
 		}
 		id, _ := r.LastInsertId()
-		(*obj).SetId(id)
+		obj.SetId(id)
 		return nil
 	})
 }
