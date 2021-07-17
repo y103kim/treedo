f0732731927232a2c06980593c68185165cc041c
Youngwan Kim <y103.kim@gmail.com>
2021-07-18 01:15:20 +0900

    migrations: fix typo, give full name and AUTOINCREMENT to ids

--- database/migrations.go
+++ database/migrations.go
@@ -13,7 +13,7 @@ var migrationSequence = []func(*sql.DB) error{
 func migrationV1(db *sql.DB) error {
 	createTodoTable := `
 		CREATE TABLE todo (
-			id          INTEGER                           PRIMEARY KEY,
+			todo_id     INTEGER                           PRIMARY KEY AUTOINCREMENT,
 			title       TEXT CHECK(LENGTH(title) <= 100)  NOT NULL,
 			status      TEXT CHECK(LENGTH(status) <= 20)  NOT NULL DEFAULT "Not Started",
 			hidden      BOOLEAN                           NOT NULL DEFAULT 0,
@@ -26,12 +26,12 @@ func migrationV1(db *sql.DB) error {
 
 	createDateTable := `
 		CREATE TABLE date (
-		  id    INTEGER                        PRIMEARY KEY,
-		  date  TEXT CHECK(LENGTH(date) <= 10) NOT NULL DEFAULT (date('now')),
-		  node  INTEGER                        NOT NULL,
+		  date_id INTEGER                        PRIMARY KEY AUTOINCREMENT,
+		  date    TEXT CHECK(LENGTH(date) <= 10) NOT NULL,
+		  todo_id INTEGER                        NOT NULL,
 
-		  FOREIGN KEY(date) REFERENCES todo (id) ON DELETE CASCADE
-		  UNIQUE(date, node)
+		  FOREIGN KEY(date) REFERENCES todo (todo_id) ON DELETE CASCADE
+		  UNIQUE(date, todo_id)
 		)`
 	if _, err := db.Exec(createDateTable); err != nil {
 		return errors.Wrap(err, "Cannot create Date table")
@@ -40,7 +40,7 @@ func migrationV1(db *sql.DB) error {
 	createUpdateTrigger := `
 		CREATE TRIGGER todo_updated_time UPDATE ON todo FOR EACH ROW
 			BEGIN
-				UPDATE todo SET updated_at = (strftime('%s', 'now')) WHERE id = old.id;
+				UPDATE todo SET updated_at = (strftime('%s', 'now')) WHERE todo_id = old.todo_id;
 			END`
 	if _, err := db.Exec(createUpdateTrigger); err != nil {
 		return errors.Wrap(err, "Cannot create update trigger")
@@ -48,14 +48,14 @@ func migrationV1(db *sql.DB) error {
 
 	createEdgeTable := `
 		CREATE TABLE edge (
-		  id    INTEGER PRIMEARY KEY,
-		  head  INTEGER NOT NULL,
-		  tail  INTEGER NOT NULL,
-		  root  INTEGER NOT NULL,
+		  edge_id INTEGER PRIMARY KEY AUTOINCREMENT,
+		  head    INTEGER NOT NULL,
+		  tail    INTEGER NOT NULL,
+		  root    INTEGER NOT NULL,
 
-		  FOREIGN KEY(head) REFERENCES todo (id) ON DELETE CASCADE
-		  FOREIGN KEY(tail) REFERENCES todo (id) ON DELETE CASCADE
-		  FOREIGN KEY(root) REFERENCES todo (id) ON DELETE CASCADE
+		  FOREIGN KEY(head) REFERENCES todo (todo_id) ON DELETE CASCADE
+		  FOREIGN KEY(tail) REFERENCES todo (todo_id) ON DELETE CASCADE
+		  FOREIGN KEY(root) REFERENCES todo (todo_id) ON DELETE CASCADE
 
 		  CHECK(head != tail)
 		  UNIQUE(head, tail)
