6f470bf98330f356fe662e6af0dbe83e737e3709
Youngwan Kim <y103.kim@gmail.com>
2021-08-01 00:45:03 +0900

    mapper: more clear argument name

--- database/mapper.go
+++ database/mapper.go
@@ -15,15 +15,15 @@ type Mapper interface {
 	IdFieldName() string
 }
 
-func (db *Database) Insert(obj Mapper) error {
-	table := obj.TableName()
-	obj_type := reflect.TypeOf(obj).Elem()
+func (db *Database) Insert(mapper Mapper) error {
+	table := mapper.TableName()
+	mapper_type := reflect.TypeOf(mapper).Elem()
 	field_names := make([]string, 0)
 	value_mappings := make([]string, 0)
-	for i := 0; i < obj_type.NumField(); i++ {
-		field := obj_type.Field(i)
+	for i := 0; i < mapper_type.NumField(); i++ {
+		field := mapper_type.Field(i)
 		tag := field.Tag.Get("db")
-		if tag != obj.IdFieldName() {
+		if tag != mapper.IdFieldName() {
 			field_names = append(field_names, tag)
 			value_mappings = append(value_mappings, ":"+tag)
 		}
@@ -35,19 +35,19 @@ func (db *Database) Insert(obj Mapper) error {
 		strings.Join(value_mappings, ","))
 
 	return db.Tx(func(tx *sqlx.Tx) error {
-		r, err := tx.NamedExec(cmd, obj)
+		r, err := tx.NamedExec(cmd, mapper)
 		if err != nil {
 			return errors.Wrapf(err, "Error while exec SQL\n%s\n", cmd)
 		}
 		id, _ := r.LastInsertId()
-		obj.SetId(id)
+		mapper.SetId(id)
 		return nil
 	})
 }
 
-func (db *Database) Get(obj Mapper, id int64) error {
-	table := obj.TableName()
-	pk_name := obj.IdFieldName()
+func (db *Database) Get(mapper Mapper, id int64) error {
+	table := mapper.TableName()
+	pk_name := mapper.IdFieldName()
 	cmd := fmt.Sprintf("SELECT * FROM %s WHERE %s=%d", table, pk_name, id)
-	return errors.Wrapf(db.db.Get(obj, cmd), "Error whlie select\n%s\n", cmd)
+	return errors.Wrapf(db.db.Get(mapper, cmd), "Error whlie select\n%s\n", cmd)
 }
