061939dae94ea03b05c3d278b6e53ab659df7d43
Youngwan Kim <y103.kim@gmail.com>
2021-08-03 16:36:59 +0900

    Fill Fields and Edges to Todo table

--- ent/schema/todo.go
+++ ent/schema/todo.go
@@ -1,6 +1,12 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // Todo holds the schema definition for the Todo entity.
 type Todo struct {
@@ -9,10 +15,24 @@ type Todo struct {
 
 // Fields of the Todo.
 func (Todo) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("title"),
+		field.String("status").
+			Default("Not Started"),
+		field.Bool("hidden").
+			Default(false),
+		field.Time("created_at").
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the Todo.
 func (Todo) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("child", Todo.Type).
+			From("parent"),
+	}
 }
