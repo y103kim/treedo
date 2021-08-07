37737428425186a61741d2b164b9343d5e23e02b
Youngwan Kim <y103.kim@gmail.com>
2021-08-07 12:17:36 +0900

    tree: organize in-memory tree, remove redundancy

--- treedo/tree.go
+++ treedo/tree.go
@@ -6,64 +6,55 @@ import (
 	"github.com/pkg/errors"
 	"github.com/y103kim/treedo/database"
 	"github.com/y103kim/treedo/ent"
-	"github.com/y103kim/treedo/ent/todo"
 )
 
 type Tree struct {
-	db *database.Database
+	db      *database.Database
+	Nodes   map[int]*ent.Todo
+	Parents map[int][]int
 }
 
 func CreateTree(db *database.Database) *Tree {
-	tree := &Tree{db}
+	tree := &Tree{db, nil, nil}
+	tree.Nodes = make(map[int]*ent.Todo)
+	tree.Parents = make(map[int][]int)
 	return tree
 }
 
-func (t *Tree) CreateTodo(title string) (*ent.Todo, error) {
-	var todo *ent.Todo
-	err := t.db.Tx(func(ctx context.Context, tx *ent.Tx) error {
-		if saved, err := tx.Todo.Create().SetTitle(title).Save(ctx); err != nil {
+func (t *Tree) CreateTodo(title string) error {
+	return t.db.Tx(func(ctx context.Context, tx *ent.Tx) error {
+		if res, err := tx.Todo.Create().SetTitle(title).Save(ctx); err != nil {
 			return errors.Wrapf(err, "Fail to create todo with title '%s'\n", title)
 		} else {
-			todo = saved
+			t.Nodes[res.ID] = res
 			return nil
 		}
 	})
-	return todo, err
 }
 
-func (t *Tree) GetAllTodos() ([]*ent.Todo, error) {
-	var todos []*ent.Todo
-	err := t.db.Tx(func(ctx context.Context, tx *ent.Tx) error {
-		if saved, err := tx.Todo.Query().All(ctx); err != nil {
+func (t *Tree) FetchAll() error {
+	return t.db.Tx(func(ctx context.Context, tx *ent.Tx) error {
+		if results, err := tx.Todo.Query().WithChild().All(ctx); err != nil {
 			return errors.Wrap(err, "Fail to get all todos")
 		} else {
-			todos = saved
+			for _, res := range results {
+				t.Nodes[res.ID] = res
+			}
 			return nil
 		}
 	})
-	return todos, err
 }
 
 func (t *Tree) LinkTodos(from int, to ...int) error {
 	return t.db.Tx(func(ctx context.Context, tx *ent.Tx) error {
-		if _, err := tx.Todo.UpdateOneID(from).AddChildIDs(to...).Save(ctx); err != nil {
+		if res, err := tx.Todo.UpdateOneID(from).AddChildIDs(to...).Save(ctx); err != nil {
 			return errors.Wrapf(err, "Cannot link todo %d->%v", from, to)
 		} else {
+			for _, todo_id := range to {
+				res.Edges.Child = append(res.Edges.Child, t.Nodes[todo_id])
+			}
+			t.Nodes[res.ID] = res
 			return nil
 		}
 	})
-}
-
-func (t *Tree) QueryChildren(from int) ([]*ent.Todo, error) {
-	var todos []*ent.Todo
-	err := t.db.Tx(func(ctx context.Context, tx *ent.Tx) error {
-		cond := todo.HasParentWith(todo.ID(from))
-		if children, err := tx.Todo.Query().Where(cond).All(ctx); err != nil {
-			return errors.Wrapf(err, "Cannot query children of todo from %d", from)
-		} else {
-			todos = children
-			return nil
-		}
-	})
-	return todos, err
 }
--- treedo/tree_test.go
+++ treedo/tree_test.go
@@ -23,31 +23,20 @@ func TestTransaction(t *testing.T) {
 	assert := assert.New(t)
 	db := setup(t)
 	tree := CreateTree(db)
-	todo1, err := tree.CreateTodo("Todo 1")
-	assert.Nil(err)
-	assert.Equal("Todo 1", todo1.Title)
+	assert.Nil(tree.CreateTodo("Todo 1"))
+	assert.Equal("Todo 1", tree.Nodes[1].Title)
 
-	todo_korean, err := tree.CreateTodo("한글 테스트")
-	assert.Nil(err)
-	assert.Equal("한글 테스트", todo_korean.Title)
-
-	todos, err := tree.GetAllTodos()
-	assert.Nil(err)
-	assert.Len(todos, 2)
-	assert.Equal(todos[0].Title, todo1.Title)
-	assert.Equal(todos[0].ID, 1)
-	assert.Equal(todos[1].Title, todo_korean.Title)
-	assert.Equal(todos[1].ID, 2)
+	assert.Nil(tree.CreateTodo("할일 2"))
+	assert.Equal("할일 2", tree.Nodes[2].Title)
 
 	assert.Nil(tree.LinkTodos(1, 2))
-	children, err := tree.QueryChildren(1)
-	assert.Equal(children[0].Title, todo_korean.Title)
+	children := tree.Nodes[1].Edges.Child
 	assert.Equal(children[0].ID, 2)
 
-	_, err = tree.CreateTodo("Todo 3")
-	_, err = tree.CreateTodo("Todo 4")
+	assert.Nil(tree.CreateTodo("Todo 3"))
+	assert.Nil(tree.CreateTodo("Todo 4"))
 	assert.Nil(tree.LinkTodos(2, 3, 4))
-	children, err = tree.QueryChildren(2)
+	children = tree.Nodes[2].Edges.Child
 	assert.Equal(children[0].ID, 3)
 	assert.Equal(children[1].ID, 4)
 
