dcf219ad7d777564b5942f5f4a57780c3ee4ea3c
Youngwan Kim <y103.kim@gmail.com>
2021-08-09 20:13:06 +0900

    tools: IntSet and IntSets as hashSet

---
+++ tools/set.go
@@ -0,0 +1,61 @@
+package tools
+
+type IntSet struct {
+	data map[int]bool
+}
+
+func CreateIntSet() *IntSet {
+	int_set := &IntSet{nil}
+	int_set.data = make(map[int]bool)
+	return int_set
+}
+
+func (s *IntSet) Add(arr ...int) {
+	for _, v := range arr {
+		s.data[v] = true
+	}
+}
+
+func (s *IntSet) Erase(arr ...int) {
+	for _, v := range arr {
+		delete(s.data, v)
+	}
+}
+
+func (s *IntSet) Check(v int) bool {
+	return s.data[v] == true
+}
+
+type IntSets struct {
+	data map[int]*IntSet
+}
+
+func CreateIntSets() *IntSets {
+	int_sets := &IntSets{nil}
+	int_sets.data = make(map[int]*IntSet)
+	return int_sets
+}
+
+func (s *IntSets) Add(key int, arr ...int) {
+	for _, v := range arr {
+		if _, ok := s.data[key]; !ok {
+			s.data[key] = CreateIntSet()
+		}
+		s.data[key].Add(v)
+	}
+}
+
+func (s *IntSets) Erase(key int, arr ...int) {
+	for _, v := range arr {
+		if set, ok := s.data[key]; ok {
+			set.Erase(v)
+		}
+	}
+}
+
+func (s *IntSets) Check(key, v int) bool {
+	if set, ok := s.data[key]; ok {
+		return set.Check(v)
+	}
+	return false
+}
---
+++ tools/set_test.go
@@ -0,0 +1,27 @@
+package tools
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntSets(t *testing.T) {
+	assert := assert.New(t)
+	s := CreateIntSets()
+	s.Add(10, 1, 2, 3, 4, 5)
+	s.Add(10, 1, 3, 5, 8)
+	s.Erase(10, 3, 4, 5, 6, 7)
+	s.Add(11, 1, 2, 3, 4, 5)
+	s.Add(11, 1, 3, 5, 8)
+	s.Erase(11, 3, 4, 5, 6, 7)
+	assert.Equal(true, s.Check(10, 1))
+	assert.Equal(false, s.Check(10, 3))
+	assert.Equal(false, s.Check(10, 10))
+	assert.Equal(true, s.Check(10, 8))
+
+	assert.Equal(true, s.Check(11, 1))
+	assert.Equal(false, s.Check(11, 3))
+	assert.Equal(false, s.Check(11, 10))
+	assert.Equal(true, s.Check(11, 8))
+}
